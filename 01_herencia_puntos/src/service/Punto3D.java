4c903480285d7d27a767b976f814f98df58f4b24
serraguti <pacoserranox@gmail.com>
2024-04-10 13:44:34 +0200

    commit 10/4/2024 - 13:45

---
+++ 01_herencia_puntos/src/service/Punto3D.java
@@ -0,0 +1,22 @@
+package service;
+
+public class Punto3D extends Punto{
+	private int z;
+	public Punto3D(int z) {
+		//super();//llamada a constructor sin parámetros de la superclase
+		this.z=z;
+	}
+
+	public Punto3D(int x, int y, int z) {
+		super(x, y);
+		this.z = z;
+	}
+
+	public int getZ() {
+		return z;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+}
