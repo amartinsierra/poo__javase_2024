0bb57deec284c21776a457b65ebeacf6e3ade9a2
serraguti <pacoserranox@gmail.com>
2024-04-17 13:57:02 +0200

    17-4-24 14:00

--- 16_operaciones_datos_lambdas/src/service/OperacionesService.java
+++ 16_operaciones_datos_lambdas/src/service/OperacionesService.java
@@ -1,6 +1,10 @@
 package service;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class OperacionesService {
 /*	//método que recibe una lista de números enteros
@@ -31,13 +35,36 @@ public class OperacionesService {
 	//método que recibe una lista de número y una condición
 	//y devuelve la suma de los números que cumplen esa condición
 	
-	public int sumarPorCondicion(List<Integer> numeros,Condicion condicion) {
+	public int sumarPorCondicion(List<Integer> numeros,Predicate<Integer> condicion) {
 		int suma=0;
 		for(Integer n:numeros) {
 			if(condicion.test(n)) {
 				suma+=n;
 			}
 		}
+		return suma;
+	}
+	
+	//método que recibe una lista de números e imprime los pares
+	//método que recibe un conjunto de números y guarda en un fichero los positivos
+	//método que recibe una lista y guarda en un fichero los múltiplos de cinco
+	//método que recibe un conjunto de números e imprime los negativos 
+	public void procesarPorCondicion(Collection<Integer> numeros,
+									Consumer<Integer> operacion,
+									Predicate<Integer> condicion) {
+		for(Integer n:numeros) {
+			if(condicion.test(n)) {
+				operacion.accept(n);
+			}
+		}
+	}
+	
+	public int sumaConExtra(List<Integer> numeros,Supplier<Integer> sup) {
+		int suma=0;
+		for(Integer n:numeros) {
+			suma+=n;
+		}
+		suma+=sup.get();
 		return suma;
 	}
 }
--- 16_operaciones_datos_lambdas/src/view/AppNumeros.java
+++ 16_operaciones_datos_lambdas/src/view/AppNumeros.java
@@ -1,8 +1,11 @@
 package view;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
+import java.util.function.Predicate;
 
-import service.Condicion;
 import service.OperacionesService;
 /*class Positivos implements Condicion{
 	@Override
@@ -23,9 +26,27 @@ public class AppNumeros {
 		//imprimir, por una lado, la suma de los pares
 		//y por otro la suma de los positivos
 		OperacionesService service=new OperacionesService();
-		Condicion cond=(Integer n)->n>0 ;
-		System.out.println("Suma positivos: "+service.sumarPorCondicion(numeros,cond));
+		//Condicion cond=(Integer n)->n>0 ;
+		System.out.println("Suma positivos: "+service.sumarPorCondicion(numeros,n->n>0));
 		System.out.println("Suma pares: "+service.sumarPorCondicion(numeros, n->n%2==0));
+		//suma los negativos mayores que -5
+		System.out.println("Otra: "+service.sumarPorCondicion(numeros, n->n<0&&n>-5));
+		Predicate<Integer> negativo=n->n<0;
+		System.out.println("Otra: "+service.sumarPorCondicion(numeros, negativo.and(n->n<-5)));
+		
+		
+		//imprimir los postivos
+		service.procesarPorCondicion(numeros, n->System.out.println(n), n->n>0);
+		
+		//guarde los pares en un fichero
+		service.procesarPorCondicion(numeros, n->{
+			try(FileOutputStream fos=new FileOutputStream("pares.txt",true);
+					PrintStream out=new PrintStream(fos)){
+				out.println(n);
+			}catch(IOException ex) {
+				ex.printStackTrace();
+			}
+		}, n->n%2==0);
 	}
 
 }
